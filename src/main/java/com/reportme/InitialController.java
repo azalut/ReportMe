fb11daabb831f50e567afad359cc522de80bc66f
Maciek <azalut@gmail.com>
2015-05-17 22:48:24 +0200

    inital project created; template inheritance implementing in progress now

---
+++ src/main/java/com/reportme/InitialController.java
@@ -0,0 +1,12 @@
+package com.reportme;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class InitialController {
+    @RequestMapping(value = "/")
+    public String home() {
+        return "home";
+    }
+}
