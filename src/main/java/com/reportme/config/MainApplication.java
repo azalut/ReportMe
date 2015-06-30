d8f3d2dfea4e6d14c740620dd2f630e8ad7408c5
Maciek <azalut@gmail.com>
2015-06-30 19:07:06 +0200

    migration from xml to javaconfig 2

--- src/main/java/com/reportme/config/MainApplication.java
+++ src/main/java/com/reportme/config/MainApplication.java
@@ -1,4 +1,15 @@
 package com.reportme.config;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableAutoConfiguration
+@ComponentScan("com.reportme")
 public class MainApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MainApplication.class, args);
+    }
 }
--- src/main/java/com/reportme/controller/MainController.java
+++ src/main/java/com/reportme/controller/MainController.java
@@ -1,4 +1,12 @@
 package com.reportme.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class MainController {
+    @RequestMapping(value = {"", "/home"})
+    public String home(){
+        return "home";
+    }
 }
