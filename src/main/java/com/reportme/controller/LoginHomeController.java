322495b1a8a291ba64069727c34cb6766fd51acd
Maciek <azalut@gmail.com>
2015-07-14 11:44:36 +0200

    email sending bean added; javax.mail and com.sun.mail dependencies added; properties file created; gmail email created: reportmemail at gmail.com

--- src/main/java/com/reportme/controller/LoginHomeController.java
+++ src/main/java/com/reportme/controller/LoginHomeController.java
@@ -3,7 +3,6 @@ package com.reportme.controller;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class LoginHomeController {
